78020f2140e546104024c155cd52d2bb09702ecc
Israel de Souza <israellimas@hotmail.com>
2025-04-04 05:38:30 -0300

    feat: add delete endpoint para produtos

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import getProduto from './model/getProduto.js'
 import getProdutoById from './model/getProdutoById.js'
 import postProduto from './model/postProduto.js'
 import updateProduto from './model/updateProduto.js'
+import deleteProduto from './model/deleteProduto.js'
 
 const app = express();
 app.use(express.json())
@@ -11,6 +12,7 @@ app.use('/produtos', getProdutoById)
 app.use('/produtos', getProduto)
 app.use('/produtos', postProduto)
 app.use('/produtos', updateProduto)
+app.use('/produtos', deleteProduto)
 
 
 export default app;
