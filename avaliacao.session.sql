4ad38c02b7d5517f96e6758cd7f750cbe6f2d658
Israel de Souza <israellimas@hotmail.com>
2025-04-04 03:39:14 -0300

    feat: criação db

---
+++ avaliacao.session.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE produtos;
+USE produtos;
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    descricao TEXT NOT NULL,
+    estoque INT NOT NULL
+);
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+);
