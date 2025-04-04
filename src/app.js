ba028be9e253817008bbd62ea7baf29df707801d
Israel de Souza <israellimas@hotmail.com>
2025-04-04 05:33:27 -0300

    feat: add updateProduto para o endpoint produto

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import express from 'express'
 import getProduto from './model/getProduto.js'
 import getProdutoById from './model/getProdutoById.js'
 import postProduto from './model/postProduto.js'
+import updateProduto from './model/updateProduto.js'
 
 const app = express();
+app.use(express.json())
 
 app.use('/produtos', getProdutoById)
 app.use('/produtos', getProduto)
 app.use('/produtos', postProduto)
+app.use('/produtos', updateProduto)
 
 
 export default app;
---
+++ src/model/updateProduto.js
@@ -0,0 +1,33 @@
+import express from "express";
+import connectionDB from "../config/connectionDB.js";
+
+const router = express.Router();
+
+router.put("/:id", (req, res) => {
+
+   const { id } = req.params;
+   const { nome, preco, descricao, estoque } = req.body
+
+   if (!nome || !preco || !descricao || !estoque) {
+    return res.status(400).json({ error: "Todos os campos são obrigatórios." });
+  }
+
+  const query = "UPDATE produtos SET nome = ?, preco = ?, descricao = ?, estoque = ? WHERE id = ?";
+  const values = [nome, preco, descricao, estoque, id];
+
+  connectionDB.query(query, values, (err, result) => {
+    if (err) {
+        console.error("Erro ao tentar atualizar o produto:", err.message);
+        return res.status(500).json({ error: "Erro ao tentar atualizar o produto no banco de dados."
+    });
+    }
+
+    if (result.length === 0) {
+      return res.status(404).json({ message: "Produto não encontrado." });
+    }
+
+    res.status(200).json({ message: "Produto atualizado com sucesso!" });
+  });
+});
+
+export default router;
